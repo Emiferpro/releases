1cac7b3eb2b40201d6226a12369147f868dc00bf
Emiferpro <emiferpro@gmail.com>
2023-05-31 01:06:09 -0300

    viva: Update OTA JSON
    Signed-off-by: Emiferpro <emiferpro@gmail.com>

--- upload.sh
+++ upload.sh
@@ -3,4 +3,7 @@ buildprop=/home/emiferpro/risingOS/out/target/product/$device/system/build.prop
 linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
 timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
 
-gh release create $timestamp rising* boot.img
+gh release create  -F changelog_viva.txt --title "RisingOS for Redmi Note 11 Pro 4G (viva) | $timestamp" $timestamp rising* boot.img
+git add .
+git commit -sm "viva: Update OTA JSON"
+git push
